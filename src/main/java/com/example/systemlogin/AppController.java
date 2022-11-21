f530800ded158cfe4c25b2e1a0456b22b7dc4568
Yousif <yjasm8959@gmail.com>
2022-11-22 02:04:58 +0300

    Initial commit

---
+++ src/main/java/com/example/systemlogin/AppController.java
@@ -0,0 +1,2 @@
+package com.example.systemlogin;public class AppController {
+}
---
+++ src/main/java/com/example/systemlogin/SystemLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemLoginApplication.class, args);
+	}
+
+}
